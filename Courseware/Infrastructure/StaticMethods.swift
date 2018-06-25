a1bdbc504fef001bee57c1c040fbb75b57807395
ATIS <ayersprogramming@gmail.com>
2018-06-25 11:04:05 -0700

    Added static methods for app-wide usage

---
+++ Courseware/Infrastructure/StaticMethods.swift
@@ -0,0 +1,35 @@
+//
+//  StaticMethods.swift
+//  Courseware
+//
+//  Created by John Ayers on 6/25/18.
+//  Copyright © 2018 Blacksmith Developers. All rights reserved.
+//
+
+import UIKit
+
+enum StaticMethods {
+    static func doesWebDirectoryExist(_ dir: String) -> Bool {
+        let combined = documentsDirectory.appendingPathComponent(dir, isDirectory: true)
+        do {
+            _ = try FileManager.default.contentsOfDirectory(at: combined, includingPropertiesForKeys: nil)
+            return true
+        } catch {
+            return false
+        }
+    }
+    
+    static func createJavaScriptFile(lessonName: String) -> Bool{
+        let js = "$(function(){window.location.href = \"Index.html?lessonName=\(lessonName)\";});"
+        let file = documentsDirectory.appendingPathComponent("lesson.js")
+        do {
+            try js.write(to: file, atomically: false, encoding: .utf8)
+        } catch {
+            print("error writing file: \(error)")
+            return false
+        }
+        return true
+    }
+    
+    static let documentsDirectory = (FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)).first!
+}
