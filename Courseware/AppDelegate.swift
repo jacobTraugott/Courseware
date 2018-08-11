fc34e2ba3d69c71d0e8b81c0db7eee777660101e
ATIS <ayersprogramming@gmail.com>
2018-08-10 23:43:32 -0500

    Added capability to receive file from share sheet and open within the web view.

--- Courseware/AppDelegate.swift
+++ Courseware/AppDelegate.swift
@@ -18,6 +18,20 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Override point for customization after application launch.
         return true
     }
+    
+    func application(_ app: UIApplication, open url: URL, options: [UIApplicationOpenURLOptionsKey : Any] = [:]) -> Bool {
+        if let homeURL = CourseCatelog.openCourse(fromURL: url) {
+            let storyboard = UIStoryboard(name: "Main", bundle: nil)
+            let webVC: WebViewController = storyboard.instantiateViewController(withIdentifier: "webViewForCourse") as! WebViewController
+            webVC.lessonURL = homeURL
+            webVC.openedFromAppDelegate = true
+            window?.rootViewController = webVC
+            window?.makeKeyAndVisible()
+            return true
+        }
+        
+        return false
+    }
 
     func applicationWillResignActive(_ application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
